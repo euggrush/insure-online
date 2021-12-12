f0dd4aaa06fe197cce989dd47ce6a3832765bff3
evgenii.grushin <evgenii.grushin@fortify.pro>
2021-12-12 01:02:49 -0800

    add3

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:euggrush/euggrush.github.io.git gh-pages
+git push -f git@github.com:euggrush/euggrush.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:euggrush/insure-online.git master:gh-pages
+#git push -f git@github.com:euggrush/insure-online.git master:gh-pages
 
 cd -
