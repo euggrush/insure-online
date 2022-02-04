c9ca89e1a1779c425fb5ef706634a4fc24e89616
evgenii.grushin <evgenii.grushin@fortify.pro>
2022-02-04 13:08:02 -0800

    add

--- src/views/AdminPanel/AccountInfo.vue
+++ src/views/AdminPanel/AccountInfo.vue
@@ -202,130 +202,8 @@
     <!-- CREATE VEHICLE FORN -->
     <div>
       <div class="collapse collapse-horizontal" id="collapseWidthExample">
-        <div class="card card-body" style="width: 300px">
-          <form @submit.prevent="createVehicle">
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label"
-                >Make and model:</label
-              >
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                v-model="vehicleInfo.details"
-                minlength="5"
-                maxlength="30"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail2" class="form-label">Year:</label>
-              <input
-                type="number"
-                class="form-control"
-                id="exampleInputEmail2"
-                v-model="vehicleInfo.year"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label"
-                >Registration license plate:</label
-              >
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputPassword1"
-                v-model="vehicleInfo.regNumber"
-                minlength="2"
-                maxlength="10"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword2" class="form-label">VIN:</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputPassword2"
-                v-model="vehicleInfo.vin"
-                minlength="17"
-                maxlength="17"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword3" class="form-label"
-                >Engine size:</label
-              >
-              <input
-                type="number"
-                class="form-control"
-                id="exampleInputPassword3"
-                v-model="vehicleInfo.engine"
-                step="0.1"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword4" class="form-label"
-                >Retail value:</label
-              >
-              <input
-                type="number"
-                class="form-control"
-                id="exampleInputPassword4"
-                v-model="vehicleInfo.retailValue"
-                required
-              />
-            </div>
-            <div class="mb-3">
-              <span>Tracking device:</span>
-              <label class="ms-3">
-                <input
-                  type="radio"
-                  name="tracking"
-                  value="Yes"
-                  v-model="vehicleInfo.trackingDevice"
-                  required
-                />
-                Yes
-              </label>
-              <label class="ms-3">
-                <input
-                  type="radio"
-                  name="tracking"
-                  value="No"
-                  v-model="vehicleInfo.trackingDevice"
-                />
-                No
-              </label>
-            </div>
-            <div class="mb-3">
-              <span>Use:</span>
-              <label class="ms-3">
-                <input
-                  type="radio"
-                  name="use"
-                  value="Private"
-                  v-model="vehicleInfo.useCase"
-                  required
-                />
-                Private
-              </label>
-              <label class="ms-3">
-                <input
-                  type="radio"
-                  name="use"
-                  value="Business"
-                  v-model="vehicleInfo.useCase"
-                />
-                Business
-              </label>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+        <div class="card card-body mt-3" style="width: 300px">
+          <CreateVehicleForm :myProps="{ accountId: accountId }" />
         </div>
       </div>
     </div>
@@ -582,6 +460,7 @@
 </template>
 
 <script>
+import CreateVehicleForm from "../../components/Forms/CreateVehicleForm.vue";
 const getTimeStamp = (date) => {
   let myDate = date;
   myDate = myDate.split("-");
@@ -591,21 +470,14 @@ const getTimeStamp = (date) => {
   return timestamp;
 };
 export default {
+  components: {
+    CreateVehicleForm,
+  },
   data() {
     return {
       isEdit: false,
       isRemovePopup: false,
-      vehicleInfo: {
-        accountId: ``,
-        details: ``,
-        year: ``,
-        regNumber: ``,
-        vin: ``,
-        engine: ``,
-        retailValue: ``,
-        trackingDevice: ``,
-        useCase: ``,
-      },
+
       changeVehicleObj: {},
       dateOfBirth: ``,
       changeUserObj: {
@@ -651,16 +523,7 @@ export default {
         )
         .catch((err) => alert(err));
     },
-    createVehicle() {
-      this.vehicleInfo.accountId = this.accountId;
-      this.$store
-        .dispatch(`CREATE_VEHICLE`, this.vehicleInfo)
-        .then(
-          this.$store.dispatch(`GET_USERS`, `?accountId=${this.accountId}`),
-          (this.vehicleInfo = {})
-        )
-        .catch((error) => alert(error));
-    },
+
     openEditVehicle(vehicle, index) {
       this.changeVehicleObj = {
         vehicleId: vehicle.vehicleId,
