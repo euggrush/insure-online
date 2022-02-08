7c5acf1b7a384ca3ec1da29b46b95f181538f643
evgenii.grushin <evgenii.grushin@fortify.pro>
2022-02-07 21:57:58 -0800

    фвв

--- src/components/Forms/DateRange.vue
+++ src/components/Forms/DateRange.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="d-flex flex-wrap justify-content-between align-items-sm-center">
-    <span class="me-1">From:</span>
-    <Datepicker v-model="dateFrom" />
-    <span class="ms-1 me-1">To:</span>
-    <Datepicker v-model="dateTo" />
-    <button type="button" class="btn btn-dark ms-1 me-1" @click="getRange">
-      Show
-    </button>
-    <button
-      type="button"
-      class="btn btn-outline-secondary ms-1"
-      @click="resetRange"
-    >
-      Reset
-    </button>
+  <div
+    class="
+      d-flex
+      flex-wrap
+      justify-content-between
+      align-items-center
+      range-wrap
+    "
+  >
+    <div class="d-flex justify-content-end align-items-center from-wrap">
+      <span class="me-1">From:</span>
+      <Datepicker v-model="dateFrom" />
+    </div>
+    <div class="d-flex justify-content-end align-items-center to-frap">
+      <span class="ms-1 me-1">To:</span>
+      <Datepicker v-model="dateTo" />
+    </div>
+    <div class="d-flex justify-content-end align-items-center btns-wrap">
+      <button type="button" class="btn btn-dark ms-1 me-1" @click="getRange">
+        Show
+      </button>
+      <button
+        type="button"
+        class="btn btn-outline-secondary ms-1"
+        @click="resetRange"
+      >
+        Reset
+      </button>
+    </div>
   </div>
 </template>
 
@@ -51,5 +65,22 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.range-wrap {
+  width: 100%;
+  margin-top: 1em;
+  @include media-breakpoint-up(lg) {
+    width: auto;
+  }
+}
+.from-wrap,
+.to-frap,
+.btns-wrap {
+  width: 100%;
+  margin-top: 1em;
+  @include media-breakpoint-up(md) {
+    width: auto;
+    margin-top: 0;
+  }
+}
 </style>
