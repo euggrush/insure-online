f30ab92b068f0ca39f26f6918c4619e98eb20638
evgenii.grushin <evgenii.grushin@fortify.pro>
2022-02-09 18:47:31 -0800

    add

--- src/views/AdminPanel/EstimationsList.vue
+++ src/views/AdminPanel/EstimationsList.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="container estimations-list-wrapper mt-3">
     <h3>Quotes:</h3>
+    <div class="date-range-wrap">
+      <DateRange class="mt-0" />
+    </div>
     <ul class="list-group mt-3">
       <li
         v-for="(estimation, index) in estimationsList"
@@ -47,7 +50,7 @@
             >
           </div>
 
-          <div class="col border-bottom border-start">
+          <div class="col col-lg-4 border-bottom border-start">
             <span class="d-block fw-bold text-decoration-underline"
               >Client:</span
             >
@@ -60,7 +63,7 @@
                 text-decoration-none
                 email-link
                 text-black
-                border-end border-bottom border-dark
+                border-bottom border-dark
               "
               :href="`mailto:${estimation.email}`"
               >&nbsp;{{ estimation.email }}</a
@@ -73,7 +76,7 @@
                 text-black
                 mt-1
                 mb-1
-                border-end border-bottom border-dark
+                border-bottom border-dark
               "
               :href="`tel:${estimation.cellphone}`"
               >&nbsp;{{ estimation.cellphone }}</a
@@ -86,12 +89,27 @@
 </template>
 
 <script>
+import DateRange from "../../components/Forms/DateRange.vue";
 const dayjs = require("dayjs");
 
 export default {
+  components: {
+    DateRange,
+  },
+  watch: {
+    getTimeRange() {
+      this.$store.dispatch(
+        `GET_ESTIMATIONS`,
+        `?order=desc&createdFrom=${this.$store.state.date_range.createdFrom}&createdTo=${this.$store.state.date_range.createdTo}`
+      );
+    },
+  },
   computed: {
     estimationsList() {
       return this.$store.state.estimations.estimations;
+    },
+    getTimeRange() {
+      return this.$store.state.date_range;
     },
   },
 
@@ -126,5 +144,16 @@ export default {
   background-repeat: no-repeat;
   background-position: left center;
   padding-left: 25px;
+}
+.date-range-wrap {
+  @include media-breakpoint-up(md) {
+    max-width: 87%;
+  }
+  @include media-breakpoint-up(lg) {
+    max-width: 70%;
+  }
+  @include media-breakpoint-up(xl) {
+    max-width: 55%;
+  }
 }
 </style>
