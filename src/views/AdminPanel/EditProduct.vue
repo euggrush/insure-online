33ddf056c48462e5403b27123364d8ff9e13d567
evgenii.grushin <evgenii.grushin@fortify.pro>
2022-02-10 12:59:04 -0800

    add

--- src/views/AdminPanel/EditProduct.vue
+++ src/views/AdminPanel/EditProduct.vue
@@ -1,29 +1,18 @@
 <template>
   <section>
-    <p class="fw-bold">Product details:</p>
+    <h2 class="d-block fw-bold text-capitalize fs-4 mb-0">
+      {{ product.mainProductName }}
+    </h2>
 
-    <span class="d-block fw-bold pb-3">{{ product.mainProductName }}</span
-    ><br />
-    <span class="d-inline-block fw-bolder text-decoration-underline mt-1"
-      >Description:&nbsp;</span
-    >
-    <span>{{ product.mainProductDescription }}</span
-    ><br />
-    <span class="d-inline-block fw-bolder text-decoration-underline mt-1"
-      >Cost:&nbsp;</span
-    >
-    <span>{{ product.mainProductCost }}</span
-    ><br />
-    <span class="d-inline-block fw-bolder text-decoration-underline mt-1"
-      >Category:&nbsp;</span
-    >
-    <span>{{ product.categoryName }}</span
+    <strong class="d-block text-capitalize mt-3">{{
+      product.mainProductDescription
+    }}</strong>
+
+
+    <span class="d-block mt-3"><strong>R</strong>{{ product.mainProductCost }}</span
     ><br />
 
-    <span class="d-inline-block fw-bolder text-decoration-underline mt-1"
-      >Created:&nbsp;</span
-    >
-    <span>{{ getDate(product.created) }}</span
+    <em class="d-block mt-3">Created At&nbsp;{{ getDate(product.created) }}</em
     ><br />
 
     <p class="mt-3 fw-bold">Included:</p>
@@ -37,11 +26,58 @@
         <span class="fw-bold">R{{ coverage.subProductCost }}</span>
         <button
           type="button"
-          class="btn btn-outline-danger float-end"
+          class="btn btn-outline-info"
+          @click="
+            toggleEditCoverageModal(
+              product.mainProductId,
+              coverage.subProductId
+            )
+          "
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          class="btn btn-outline-danger"
           @click="removeCoverage(product.mainProductId, coverage.subProductId)"
         >
           Remove
         </button>
+        <Transition>
+          <div v-if="show === coverage.subProductId">
+            <form
+              class="mt-5 mb-5 p-3 border border-secondary border-2"
+              @submit.prevent="editCoverage"
+            >
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label"
+                  >Coverage Name</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="name"
+                  v-model="editCoveragePayload.subProductName"
+                  :placeholder="coverage.subProductName"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label"
+                  >Coverage Premium</label
+                >
+                <input
+                  type="number"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  v-model="editCoveragePayload.cost"
+                  :placeholder="coverage.subProductCost"
+                />
+              </div>
+              <button type="submit" class="btn btn-info">Submit</button>
+            </form>
+          </div>
+        </Transition>
       </li>
     </ul>
 
@@ -220,27 +256,25 @@ const dayjs = require("dayjs");
 export default {
   data() {
     return {
+      show: false,
       isSubProductsError: false,
       checkedCoverages: [],
-      productName: ``,
-      pickedProductIndex: ``,
-      pickedProductInfo: [],
       pickedProductId: ``,
-      productCategoryId: ``,
-      productNewName: ``,
-      isBtnDisabled: true,
       mainProductName: ``,
       mainProductDescription: ``,
       cost: ``,
       isRequiredCoverages: ``,
-      isCategorySelected: false,
       mainProductId: ``,
-      productInfo: {},
       subProductName: ``,
       subProductId: ``,
       subProductDescription: ``,
       coverageCost: ``,
       availableCoveragesToAdd: [],
+      editCoveragePayload: {
+        subProductId: ``,
+        subProductName: ``,
+        cost: ``,
+      },
     };
   },
   computed: {
@@ -403,6 +437,26 @@ export default {
           )
         );
     },
+    toggleEditCoverageModal(productId, coverageId) {
+      this.pickedProductId = productId;
+      if (!this.show) {
+        this.show = coverageId;
+      } else {
+        this.show = false;
+      }
+      this.editCoveragePayload.subProductId = coverageId;
+    },
+    editCoverage() {
+      this.$store
+        .dispatch(`CREATE_SUB_PRODUCT`, this.editCoveragePayload)
+        .then(() => {
+          this.show = false;
+          this.$store.dispatch(
+            `GET_MAIN_PRODUCTS`,
+            `?mainProductId=${this.pickedProductId}`
+          );
+        });
+    },
   },
 };
 </script>
@@ -426,5 +480,26 @@ br {
 }
 .coverages-checkboxes__item label:hover {
   color: $bgOrange;
+}
+.btn-outline-info,
+.btn-outline-danger {
+  min-width: 100%;
+  margin-top: 1em;
+  @include media-breakpoint-up(md) {
+    float: right;
+    min-width: 7em;
+    margin-top: 0;
+    margin-left: 1em;
+  }
+}
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
