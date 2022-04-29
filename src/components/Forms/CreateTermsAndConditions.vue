be2dbb2762d3ee4022009e44da1e29a91c637664
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-04-29 04:14:23 -0700

    add

--- src/components/Forms/CreateTermsAndConditions.vue
+++ src/components/Forms/CreateTermsAndConditions.vue
@@ -1,19 +1,58 @@
 <template>
-  <form>
-    <div class="form-floating">
-      <textarea
-        class="form-control"
-        placeholder="Write terms and conditions here"
-        id="floatingTextarea2"
-        style="height: 100px"
-      ></textarea>
-      <label for="floatingTextarea2">terms and conditions</label>
-    </div>
-  </form>
+  <section>
+    <p class="fw-lighter fst-italic">{{ termsAndConditionsData }}</p>
+    <form @submit.prevent="createTermsAndConditions" class="mt-3">
+      <div class="form-floating">
+        <textarea
+          class="form-control"
+          :placeholder="termsAndConditionsData"
+          id="floatingTextarea2"
+          style="height: 100px"
+          v-model="termsAndConditionsText"
+        ></textarea>
+        <label for="floatingTextarea2">Terms aAnd Conditions</label>
+      </div>
+      <button type="submit" class="btn btn-secondary mt-3">Submit</button>
+    </form>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      termsAndConditionsText: ``,
+    };
+  },
+  computed: {
+    termsAndConditionsData() {
+      return this.getTermsAndConditions();
+    },
+  },
+  mounted() {
+    this.$store.dispatch(`GET_RATING`, ``);
+  },
+  methods: {
+    getTermsAndConditions() {
+      let termsAndConditions = ``;
+      this.$store.state.rating.resources.map((item) => {
+        if (item.resourceKey == `termsAndConditions`) {
+          termsAndConditions = item.resourceValue;
+        }
+      });
+      return termsAndConditions;
+    },
+    createTermsAndConditions() {
+      const payload = {
+        resourceKey: `termsAndConditions`,
+        resourceValue: this.termsAndConditionsText,
+      };
+      this.$store.dispatch(`MODIFY_RATING`, payload).then(() => {
+        this.termsAndConditionsText = ``;
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
