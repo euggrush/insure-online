ec0fa960b165f9eca1c36e355f77c896b85cbd73
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-05-18 23:06:25 -0700

    add

--- src/components/Partials/Services.vue
+++ src/components/Partials/Services.vue
@@ -8,7 +8,13 @@
         <div class="col-lg-3 col-md-6 text-center">
           <div class="mt-5">
             <div class="mb-2">
-              <i class="fas fa-truck-monster fs-1 text-primary"></i>
+              <img
+                src="../../assets/img/icon-ev.png"
+                alt="icon"
+                width="110"
+                height="100"
+              />
+              <!-- <i class="fas fa-truck-monster fs-1 text-primary"></i> -->
             </div>
             <h3 class="h4 mb-2">TUFFSTUFF 4x4 Insurance Product</h3>
             <p class="text-muted mb-0">
@@ -20,7 +26,13 @@
         <div class="col-lg-3 col-md-6 text-center">
           <div class="mt-5">
             <div class="mb-2">
-              <i class="fas fa-helicopter fs-1 text-primary"></i>
+              <img
+                src="../../assets/img/icon-helicopter.png"
+                alt="icon"
+                width="110"
+                height="100"
+              />
+              <!-- <i class="fas fa-helicopter fs-1 text-primary"></i> -->
             </div>
             <h3 class="h4 mb-2">Airborne Medical Evacuation</h3>
             <p class="text-muted mb-0">
@@ -32,7 +44,13 @@
         <div class="col-lg-3 col-md-6 text-center">
           <div class="mt-5">
             <div class="mb-2">
-              <i class="fas fa-ambulance fs-1 text-primary"></i>
+              <img
+                src="../../assets/img/icon-ev.png"
+                alt="icon"
+                width="110"
+                height="100"
+              />
+              <!-- <i class="fas fa-ambulance fs-1 text-primary"></i> -->
             </div>
             <h3 class="h4 mb-2">Emergency Accommodation</h3>
             <p class="text-muted mb-0">
@@ -44,7 +62,13 @@
         <div class="col-lg-3 col-md-6 text-center">
           <div class="mt-5">
             <div class="mb-2">
-              <i class="fas fa-balance-scale fs-1 text-primary"></i>
+              <img
+                src="../../assets/img/icon-ev.png"
+                alt="icon"
+                width="110"
+                height="100"
+              />
+              <!-- <i class="fas fa-balance-scale fs-1 text-primary"></i> -->
             </div>
             <h3 class="h4 mb-2">International Legal Assistance</h3>
             <p class="text-muted mb-0">
