dbf17685b1188a92013aaa934c220a43e59c3627
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-06-07 17:44:23 -0700

    refactoring

--- src/views/ClientSide/MyAccount/MyAccount.vue
+++ src/views/ClientSide/MyAccount/MyAccount.vue
@@ -4,24 +4,7 @@
       class="m-5 my-account_info d-flex flex-wrap justify-content-lg-between"
     >
       <div class="my-account_one d-flex flex-column align-items-center">
-        <label for="formFile" class="form-label user-avatar-wrap">
-          <img
-            id="avatar"
-            :src="`${FILE_URL}${avatar}`"
-            alt="avatar"
-            class="img user-avatar-photo img-thumbnail rounded-circle"
-            width="100"
-            height="100"
-          />
-          <input
-            type="file"
-            accept="image/*"
-            id="formFile"
-            name="asset"
-            @change="getAvatar($event)"
-            v-show="showInput"
-          />
-        </label>
+        <MyAvatar/>
         <h2 class="text-center">
           {{ myAccountInfo.firstName }} {{ myAccountInfo.lastName }}
         </h2>
@@ -91,129 +74,31 @@
 </template>
 
 <script>
-import { FILE_URL, DEFAULT_AVATAR } from "../../../constants";
 import ModalMessage from "../../../components/Modals/ModalMessage.vue";
 import EditAccountForm from "../../../components/Forms/EditForms/EditAccountForm.vue";
+import MyAvatar from "./MyAvatar.vue";
 
 export default {
   components: {
     ModalMessage,
-    EditAccountForm,
+    EditAccountForm,MyAvatar
   },
   data() {
     return {
       showAccountEdit: false,
-      FILE_URL: FILE_URL,
-      showInput: false,
-      avatar: DEFAULT_AVATAR,
-      accountId: ``,
-      
     };
   },
   computed: {
     myAccountInfo() {
       if (this.$store.state.users_array.accounts) {
-        if (this.$store.state.users_array.accounts[0].avatar !== ``) {
-          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-          this.avatar = this.$store.state.users_array.accounts[0].avatar;
-        }
+        
         return this.$store.state.users_array.accounts[0];
       }
       return [];
     },
   },
   mounted() {
-    this.accountId = this.$store.state.user.accountId;
-    this.$store.dispatch(`GET_USERS`, `?accountId=${this.accountId}`);
-  },
-  methods: {
-    getAvatar(event) {
-      let files = event.target.files;
-      let file = files[0];
-      if (file) {
-        let reader = new FileReader();
-        reader.onload = function (e) {
-          document.getElementById("avatar").src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-        setTimeout(() => {
-          this.resizeImage(file);
-        }, 1000);
-      }
-    },
-    resizeImage(file) {
-      if (file) {
-        let reader = new FileReader();
-
-        reader.onload = (e) => {
-          let img = document.createElement("img");
-          img.src = e.target.result;
-          let canvas = document.createElement("canvas");
-          let ctx = canvas.getContext("2d");
-
-          ctx.drawImage(img, 0, 0);
-
-          const MAX_WIDTH = 300;
-          const MAX_HEIGHT = 300;
-
-          let width = img.width;
-          let height = img.height;
-
-          if (width > height) {
-            if (width > MAX_WIDTH) {
-              height *= MAX_WIDTH / width;
-              width = MAX_WIDTH;
-            }
-          } else {
-            if (height > MAX_HEIGHT) {
-              width *= MAX_HEIGHT / height;
-              height = MAX_HEIGHT;
-            }
-          }
-          canvas.width = width;
-          canvas.height = height;
-
-          ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0, width, height);
-
-          let dataurl = canvas.toDataURL(file);
-          setTimeout(() => {
-            let asset = this.dataURItoBlob(dataurl);
-            this.uploadAvatar(asset);
-          }, 1000);
-        };
-        reader.readAsDataURL(file);
-      }
-    },
-    uploadAvatar(asset) {
-      const formData = new FormData();
-      formData.append(
-        `meta`,
-        JSON.stringify({
-          fileType: `avatar`,
-          description: `avatar`,
-          relatedTo: `avatar`,
-          relationId: this.accountId,
-        })
-      );
-      formData.append("asset[]", asset);
-      this.$store
-        .dispatch(`UPLOAD`, formData)
-        .then(() => {
-          setTimeout(() => {
-            this.$store.dispatch(`GET_USERS`, `?accountId=${this.accountId}`);
-          }, 1000);
-        })
-        .catch(() => {
-          this.$store.commit(`SET_MODAL`, {
-            isModal: true,
-            msg:
-              this.$store.state.general_errors ??
-              `File upload error, please try later`,
-          });
-        });
-    },
-  
+    this.$store.dispatch(`GET_USERS`, ``);
   },
 };
 </script>
@@ -247,48 +132,7 @@ export default {
 li {
   margin-bottom: 10px;
 }
-.user-avatar-wrap {
-  width: 100%;
-  height: auto;
-  cursor: pointer;
-  object-fit: cover;
-  margin-top: 10px;
-  @include media-breakpoint-up(sm) {
-    width: 180px;
-    height: 180px;
-  }
-  @include media-breakpoint-up(md) {
-    width: 230px;
-    height: 230px;
-  }
-  @include media-breakpoint-up(lg) {
-    width: 308px;
-    height: 308px;
-  }
-  @include media-breakpoint-up(xl) {
-    width: 375px;
-    height: 375px;
-  }
-  @include media-breakpoint-up(xxl) {
-    width: 445px;
-    height: 445px;
-  }
-}
-.user-avatar-wrap:hover {
-  outline: solid 1px #212529;
-  background-image: url("../../../assets/img/icon-change-avatar.png");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100px 100px;
-}
-.user-avatar-photo {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.user-avatar-photo:hover {
-  opacity: 0.3;
-}
+
 .table-dark {
   min-height: 290px;
   @include media-breakpoint-up(sm) {
